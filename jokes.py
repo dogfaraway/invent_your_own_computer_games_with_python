62c89966305dd256fa5f9dfdd56e54a851dcc556
dogfaraway <dogfaraway@hotmail.com>
2018-11-18 13:07:51 +0800

    ch4 tell jokes end='' 关键字实参传递为关键字形参，取消print自动换行符

---
+++ jokes.py
@@ -0,0 +1,22 @@
+## tell jokes
+
+print('What do you get when you cross a snowman with a vampire?')
+input()
+print('Frostbite')
+print()
+print('What do dentists call an astronaut\'s cavity?')
+input()
+print('A black hole')
+print()
+print('Knock knock.')
+input()
+print('Who\'s there?')
+print()
+print('Interrupting cow.')
+input()
+print('Interrupting cow wh', end='')
+# end关键字形参（keyword parameter）,
+# print()空字符串叫做关键字实参(keyword argument),
+# 将关键字实参传递给关键字形参，必须在前面输入end=
+# print()不会有自动换行符出现
+print('-MOO!')
